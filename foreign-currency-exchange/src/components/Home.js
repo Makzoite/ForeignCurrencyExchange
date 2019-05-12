cbd139dc1a0d219ca5987c21096baf67cbbc3b8d
maheshraj.mainali@gmail.com <maheshraj.mainali@gmail.com>
2019-05-12 16:34:20 -0400

    UI for currency conversion

--- foreign-currency-exchange/src/components/Home.js
+++ foreign-currency-exchange/src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../App.css';
-import {Button} from 'react-bootstrap';
+import {Button, Form, Col} from 'react-bootstrap';
 
 class Home extends Component {
   constructor(props) {
@@ -16,11 +16,34 @@ class Home extends Component {
       currencyType: '',
     };
     this.onConvertClickHandler = this.onConvertClickHandler.bind(this);
+    this.onTextBoxChangeCurrencyType = this.onTextBoxChangeCurrencyType.bind(this);
+    this.onTextBoxChangeCurrencyTypeToConvert = this.onTextBoxChangeCurrencyTypeToConvert.bind(this);
+    this.onTextBoxChangeAmoutToConvert = this.onTextBoxChangeAmoutToConvert.bind(this);
+  }
+  onTextBoxChangeCurrencyType(event){
+    this.setState({
+          currencyType: event.target.value,
+        });
+  }
+  onTextBoxChangeCurrencyTypeToConvert(event){
+    this.setState({
+          currencyTypeToConvert: event.target.value,
+        });
+  }
+  onTextBoxChangeAmoutToConvert(event){
+    this.setState({
+          amountToConvert: event.target.value,
+        });
   }
   onConvertClickHandler(){
-    var currencyType = 'USD';
-    var currencyTypeToConvert = 'CAD';
-    var amountToConvert = 100;
+    const {
+      isConverted,
+      exchangeRate,
+      totalConvertedAmount,
+      amountToConvert,
+      currencyTypeToConvert,
+      currencyType,
+    } = this.state;
     fetch('https://api.exchangeratesapi.io/latest?base='+currencyType)
             .then(res => res.json())
             .then(json => {
@@ -37,9 +60,7 @@ class Home extends Component {
                 });
               }
               else{
-                this.setState({
-
-                });
+                alert(json.error);
               }
             });
 
@@ -59,17 +80,37 @@ class Home extends Component {
     } = this.state;
     return (
       <div>
-        <Button variant="primary" onClick={this.onConvertClickHandler}>
-              Convert
-        </Button>
+      <h1>Currency Exchage</h1>
+      <Form>
+        <Form.Row>
+          <Col>
+            <Form.Control placeholder="Currency Type" value={currencyType} onChange = {this.onTextBoxChangeCurrencyType} />
+          </Col>
+          <Col md="1.5">
+          <Form.Label>To</Form.Label>
+          </Col>
+          <Col>
+            <Form.Control placeholder="Currency type to convert" value={currencyTypeToConvert} onChange = {this.onTextBoxChangeCurrencyTypeToConvert}/>
+          </Col>
+          <Col>
+          <Form.Control placeholder="Enter Currency" value={amountToConvert==0?"":amountToConvert} onChange = {this.onTextBoxChangeAmoutToConvert}/>
+          </Col>
+
+          <Col>
+          <Button variant="primary" onClick={this.onConvertClickHandler}>
+                Convert
+          </Button>
+          </Col>
+        </Form.Row>
+    </Form>
+
         {
           isConverted
           ?
           <div>
           <h1>Converted</h1>
-          <p>Conversion from {currencyType} to {currencyTypeToConvert}</p>
-          <p>Total converted amount is {totalConvertedAmount} from {amountToConvert}.</p>
-          <p>Exchange rate is {exchangeRate}</p>
+          <p>Exchange rate: {exchangeRate}</p>
+          <p>Converted amount: {totalConvertedAmount}</p>
           </div>
           :
           null
